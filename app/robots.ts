be18d7e9f65d4ef7bb2104415aba4feb8449f6db
garrison2025 <zhongsgeng@gmail.com>
2025-08-01 03:52:00 +0200

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: ["/api/", "/admin/"],
     },
-    sitemap: "https://graffiti-generator.vercel.app/sitemap.xml",
+    sitemap: "https://graffitischriftgenerator.pro/sitemap.xml",
   }
 }
